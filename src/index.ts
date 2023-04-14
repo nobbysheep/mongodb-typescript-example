b25209fba9119c4d0ae7977ee3cef619cb9720a6
nobbysheep <nobbysheep@outlook.com>
2023-04-14 15:11:30 +0100

    Got is working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { connectToDatabase } from "./services/database.service"
+import { gamesRouter } from "./routes/games.router";
 
 const app = express();
 const port = 8080; // default port to listen
 
+/*
 // ** TODO ** Replace this code with a call to your games router class to handle all calls to /games endpoint
 app.get("/", (req, res) => {
     res.send("Hello world!");
@@ -14,3 +17,16 @@ app.listen(port, () => {
 
     // ** TODO ** Call to Game Service to initiate connection
 });
+*/
+connectToDatabase()
+    .then(() => {
+        app.use("/games", gamesRouter);
+
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
