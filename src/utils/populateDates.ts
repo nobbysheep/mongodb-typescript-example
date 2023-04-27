857ba2d0d3038e129948fce52e42f1369f2b8b60
nobbysheep <81171320+nobbysheep@users.noreply.github.com>
2023-04-27 07:27:17 +0000

    still trying to get bloody date array working

--- src/utils/populateDates.ts
+++ src/utils/populateDates.ts
@@ -1,8 +1,7 @@
 // Populate DB with array of dates and ISO week numbers
 import { getWeekNumberFromISOString } from "../utils/getWeekNumbers";
-import { calendarDate } from "../models/schemas";
 
-export function populateDates({ dateArray }: { dateArray: calendarDate[] }): void {
+export function populateDates({ dateArray }: { dateArray: Date[] }): void {
     console.log(dateArray);
     for (let i = 0; i < dateArray.length; i++) {
         let weekNum = dateArray[i].toLocaleString();
