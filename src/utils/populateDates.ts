229a35e02c4646dc8f13ab8cc2af1b8520f441e0
nobbysheep <nobbysheep@outlook.com>
2023-05-01 20:35:06 +0100

    still trying to get populateDates working

--- src/utils/populateDates.ts
+++ src/utils/populateDates.ts
@@ -8,17 +8,19 @@ import { weekNumber } from 'weeknumber'
 export function populateDates(): (string|number)[] {
     // Build initial array with dates
     const dateArray: Date[] = getDates({
-        startDate: new Date(2022, 0, 1, 0, 0),
-        endDate: new Date(2022, 3, 31, 23, 59, 59),
+        startDate: new Date(2022, 0, 1),
+        endDate: new Date(2022, 2, 31),
     });
     // Convert dates array to ISO strings and add ISO week numbers
-    let tmpISO = dateArray[0].toISOString();
-    let weekNum = weekNumber(dateArray[0]);
-    const tmpArray: [tmpISO: string, weekNum: number] = [tmpISO, weekNum];
+    const tmpArray: [string, number] = [dateArray[0].toISOString(), weekNumber(dateArray[0])];
+    //tmpArray.push(tmpISO,weekNum);
     for (let i = 1; i < dateArray.length; i++) {
         let tmpISO = dateArray[i].toISOString();
         let weekNum = weekNumber(dateArray[i]);
         tmpArray.push(tmpISO,weekNum);
+        //console.log(i);
+        //tmpArray[i] = dateArray[i].toISOString();
+        //tmpArray[i+1] = weekNumber(dateArray[i]);
     }
     return tmpArray;
 }
